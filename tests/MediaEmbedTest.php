a187ade563dd2ab0b129f8974775bdc295a84de4
Thanh LS <thanhsm93@gmail.com>
2015-04-07 10:42:21 +0700

    add build file

---
+++ tests/MediaEmbedTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thanh
+ * Date: 07/04/2015
+ * Time: 11:11
+ */
+
+class MediaEmbedTest extends PHPUnit_Framework_TestCase {
+
+    public function test()
+    {
+    }
+
+}
